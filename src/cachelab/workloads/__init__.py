847a9ec160a78adb1057f32dc6ecd6f6cac3b04d
Princeton Afeez <princetonafeez@gmail.com>
2026-06-19 19:12:25 -0700

    Workloads utilities for CacheLab

---
+++ src/cachelab/workloads/__init__.py
@@ -0,0 +1,8 @@
+""" Workloads utilities for CacheLab """
+
+from __future__ import annotations
+
+from cachelab.workloads.benchmark import benchmark_policies
+from cachelab.workloads.runner import run_simulation
+
+__all__ = ["benchmark_policies", "run_simulation"]
